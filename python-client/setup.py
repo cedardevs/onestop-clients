e720d897722c1525459ae66fbdfb80b31a2777c1
Christopher Esterlein <christopher.esterlein@colorado.edu>
2020-04-15 10:29:09 -0600

    Update setup.py

--- python-client/setup.py
+++ python-client/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="onestop-python-", # Replace with your own username
+    name="onestop-python-client-cedardevs", # Replace with your own username
     version="0.0.1",
     author="CEDARDEVS",
     author_email="cedar.cires@colorado.edu",
