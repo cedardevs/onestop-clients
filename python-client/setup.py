b77d487cf01604798fbabc4f6216ef59c8cf99c3
StrayCat1 <christopher.esterlein@colorado.edu>
2020-04-16 15:24:17 -0600

    fix url

--- python-client/setup.py
+++ python-client/setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="onestop-python-client-cedardevs", # Replace with your own username
-    version="0.1.1",
+    version="0.1.2",
     author="CEDARDEVS",
     author_email="cedar.cires@colorado.edu",
     description="A python package for processing messages from the NOAA OneStop event stream (aka Inventory Manager).",
     long_description="This package provides subject matter experts an API to interact with the kafka topics backing OneStop.",
     long_description_content_type="text/markdown",
-    url="https://github.com/cedardevs/onestop-clients/python-client",
+    url="https://github.com/cedardevs/onestop-clients",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
