8a07e90c1d5bc8dcc1c40426163e9853394ade27
StrayCat1 <christopher.esterlein@colorado.edu>
2020-04-16 13:21:17 -0600

    increment version for logging change

--- python-client/setup.py
+++ python-client/setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="onestop-python-client-cedardevs", # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="CEDARDEVS",
     author_email="cedar.cires@colorado.edu",
-    description="A python package for consuming from the NOAA OneStop event stream (aka Inventory Manager).",
+    description="A python package for processing messages from the NOAA OneStop event stream (aka Inventory Manager).",
     long_description="This package provides subject matter experts an API to interact with the kafka topics backing OneStop.",
     long_description_content_type="text/markdown",
     url="https://github.com/cedardevs/onestop-clients/python-client",
