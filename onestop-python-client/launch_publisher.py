52a0b0acdad9a1f061d4929fc89f2fae6fbf6762
dneufeldcu <david.neufeld@noaa.gov>
2020-10-28 10:57:58 -0600

    cleanup variable names simplify dictionary assignment

--- onestop-python-client/launch_publisher.py
+++ onestop-python-client/launch_publisher.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     args = vars(parser.parse_args())
 
-    confLoc = args.pop('conf')
+    conf_loc = args.pop('conf')
 
     # "discovery":
     # {
@@ -20,13 +20,13 @@ if __name__ == '__main__':
     # Title: Yaquina Bay, OR (P210) Bathymetric Digital Elevation Model (30 meter resolution) Derived From Source Hydrographic Survey Soundings Collected by NOAA
     # FileIdentifier: gov.noaa.ngdc.mgg.dem:yaquina_bay_p210_30m
     collection_uuid = '3ee5976e-789a-41d5-9cae-d51e7b92a247'
-    content_dict = { }
-    content_dict['discovery'] = {'title': 'My New Title!',
-                                 'fileIdentifier': 'gov.noaa.osim.mgg.dem:yaquina_bay_p210_30m'
-                                }
+    content_dict = {'discovery': {'title': 'My Extra New Title!',
+                                  'fileIdentifier': 'gov.noaa.osim2.mgg.dem:yaquina_bay_p210_30m'
+                                  }
+                    }
 
     print(str(content_dict))
-    kafka_publisher = KafkaPublisher(confLoc)
-    kafka_publisher.connect()
-    kafka_publisher.publish_collection(collection_uuid, content_dict)
+    kafka_publisher = KafkaPublisher(conf_loc)
+    metadata_producer = kafka_publisher.connect()
+    kafka_publisher.publish_collection(metadata_producer, collection_uuid, content_dict)
 
