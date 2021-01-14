4e2e0fa6d621cd35934d4be2ec67b016a4ca11e7
Jeff Huynh <jehu0078@colorado.edu>
2021-01-14 13:27:23 -0700

    Code refactor

--- onestop-python-client/tests/extractor/CsbExtractorTest.py
+++ onestop-python-client/tests/extractor/CsbExtractorTest.py
@@ -1,152 +1,46 @@
 import unittest
-from moto import mock_s3
-from onestop.util.S3Utils import S3Utils
-from tests.utils import abspath_from_relative
 from onestop.extract.CsbExtractor import CsbExtractor
 
 class CsbExtractorTest(unittest.TestCase):
-    recs1 = \
-        [{
-            'eventVersion': '2.1',
-            'eventSource': 'aws:s3',
-            'awsRegion': 'us-east-1',
-            'eventTime': '2020-11-10T00:44:20.642Z',
-            'eventName': 'ObjectCreated:Put',
-            'userIdentity': {'principalId': 'AWS:AIDAUDW4MV7I5RW5LQJIO'},
-            'requestParameters': {'sourceIPAddress': '65.113.158.185'},
-            'responseElements': {'x-amz-request-id': '7D394F43C682BB87',
-                                 'x-amz-id-2': 'k2Yn5BGg7DM5fIEAnwv5RloBFLYERjGRG3mT+JsPbdX033USr0eNObqkHiw3m3x+BQ17DD4C0ErB/VdhYt2Az01LJ4mQ/aqS'},
-            's3': {'s3SchemaVersion': '1.0', 'configurationId': 'csbS3notification',
-                   'bucket': {'name': 'nesdis-ncei-csb-dev',
-                              'ownerIdentity': {'principalId': 'A3PGJENIF5D10L'},
-                              'arn': 'arn:aws:s3:::nesdis-ncei-csb-dev'},
-                   'object': {'key': 'csv/file1.csv', 'size': 1385,
-                              'eTag': '44d2452e8bc2c8013e9c673086fbab7a',
-                              'versionId': 'q6ls_7mhqUbfMsoYiQSiADnHBZQ3Fbzf',
-                              'sequencer': '005FA9E26498815778'}
-                   }
-        }]
-
-
     def setUp(self):
         print("Set it up!")
-        self.s3_utils = S3Utils(abspath_from_relative(__file__, "../../config/aws-util-config-dev.yml"),
-                                abspath_from_relative(__file__, "../../config/credentials-template.yml"))
-        self.csb_extractor = CsbExtractor(abspath_from_relative(__file__, "../../config/csb-data-stream-config.yml"),
-                                     self.s3_utils)
+        file_name = '../data/file4.csv'
+        self.csb_extractor = CsbExtractor(file_name)
+
 
     def tearDown(self):
         print("Tear it down!")
 
-    def test_csv(self):
-        key1 = self.recs1[0]['s3']['object']['key']
+    def test_is_csv(self):
         csv_str= '.csv'
-        self.assertTrue(key1.endswith(csv_str))
+        self.assertTrue(self.csb_extractor.is_csv(self.csb_extractor.file_name))
 
-    @mock_s3
-    def test_get_line(self):
-        s3 = self.s3_utils.connect('s3', self.s3_utils.conf['s3_region'])
-        location = {'LocationConstraint': self.s3_utils.conf['s3_region']}
-        bucket = 'nesdis-ncei-csb-dev'
-        key = 'csv/file4.csv'
 
-        with open('../data/file4.csv', 'r') as file:
-            data = file.read()
-
-        s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
-        s3.put_object(Bucket=bucket, Key=key, Body=data)
-
-        line = self.csb_extractor.get_line(bucket, key)
-
-        self.assertTrue(line != None)
-
-    @mock_s3
     def test_get_max_numeric(self):
-        s3 = self.s3_utils.connect('s3', self.s3_utils.conf['s3_region'])
-        location = {'LocationConstraint': self.s3_utils.conf['s3_region']}
-        bucket = 'nesdis-ncei-csb-dev'
-        key = 'csv/file4.csv'
-
-        with open('../data/file4.csv', 'r') as file:
-            data = file.read()
-
-        s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
-        s3.put_object(Bucket=bucket, Key=key, Body=data)
-
-        line = self.csb_extractor.get_line(bucket, key)
-        max_lon = self.csb_extractor.get_max_numeric(line, 'LON')
+        max_lon = self.csb_extractor.get_max_numeric( 'LON')
         self.assertEqual(max_lon, -92.747995)
 
-    @mock_s3
     def test_get_min_numeric(self):
-        s3 = self.s3_utils.connect('s3', self.s3_utils.conf['s3_region'])
-        location = {'LocationConstraint': self.s3_utils.conf['s3_region']}
-        bucket = 'nesdis-ncei-csb-dev'
-        key = 'csv/file4.csv'
-
-        with open('../data/file4.csv', 'r') as file:
-            data = file.read()
-
-        s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
-        s3.put_object(Bucket=bucket, Key=key, Body=data)
-
-        line = self.csb_extractor.get_line(bucket, key)
-        min_lon = self.csb_extractor.get_min_numeric(line, 'LON')
+        min_lon = self.csb_extractor.get_min_numeric( 'LON')
         self.assertEqual(min_lon, -96.847995)
 
-    @mock_s3
+
     def test_get_max_datetime(self):
-        s3 = self.s3_utils.connect('s3', self.s3_utils.conf['s3_region'])
-        location = {'LocationConstraint': self.s3_utils.conf['s3_region']}
-        bucket = 'nesdis-ncei-csb-dev'
-        key = 'csv/file4.csv'
-
-        with open('../data/file4.csv', 'r') as file:
-            data = file.read()
-
-        s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
-        s3.put_object(Bucket=bucket, Key=key, Body=data)
-
-        line = self.csb_extractor.get_line(bucket, key)
-        end_date = self.csb_extractor.get_max_datetime(line, 'TIME')
+        end_date = self.csb_extractor.get_max_datetime( 'TIME')
         self.assertEqual(end_date, '2020-04-10T14:00:06.000Z')
 
-    @mock_s3
+
     def test_get_min_datetime(self):
-        s3 = self.s3_utils.connect('s3', self.s3_utils.conf['s3_region'])
-        location = {'LocationConstraint': self.s3_utils.conf['s3_region']}
-        bucket = 'nesdis-ncei-csb-dev'
-        key = 'csv/file4.csv'
-
-        with open('../data/file4.csv', 'r') as file:
-            data = file.read()
-
-        s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
-        s3.put_object(Bucket=bucket, Key=key, Body=data)
-
-        line = self.csb_extractor.get_line(bucket, key)
-        begin_date = self.csb_extractor.get_min_datetime(line, 'TIME')
+        begin_date = self.csb_extractor.get_min_datetime( 'TIME')
         self.assertEqual(begin_date, '2018-04-10T14:00:06.000Z')
 
-    @mock_s3
+
     def test_extract_coords(self):
-        s3 = self.s3_utils.connect('s3', self.s3_utils.conf['s3_region'])
-        location = {'LocationConstraint': self.s3_utils.conf['s3_region']}
-        bucket = 'nesdis-ncei-csb-dev'
-        key = 'csv/file4.csv'
-
-        with open('../data/file4.csv', 'r') as file:
-            data = file.read()
-
-        s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
-        s3.put_object(Bucket=bucket, Key=key, Body=data)
-
-        line = self.csb_extractor.get_line(bucket, key)
-        max_lon = self.csb_extractor.get_max_numeric(line, 'LON')
-        min_lon = self.csb_extractor.get_min_numeric(line, 'LON')
-        max_lat = self.csb_extractor.get_max_numeric(line, 'LAT')
-        min_lat = self.csb_extractor.get_min_numeric(line, 'LAT')
-        coords = self.csb_extractor.extract_coords(line, max_lon, max_lat, min_lon, min_lat)
+        max_lon = self.csb_extractor.get_max_numeric('LON')
+        min_lon = self.csb_extractor.get_min_numeric('LON')
+        max_lat = self.csb_extractor.get_max_numeric('LAT')
+        min_lat = self.csb_extractor.get_min_numeric('LAT')
+        coords = self.csb_extractor.extract_coords(max_lon, max_lat, min_lon, min_lat)
         result = [[
             -94.847995,
             29.373065
@@ -165,6 +59,7 @@ class CsbExtractorTest(unittest.TestCase):
             ]
         ]
         self.assertEqual(coords, result)
+
 
 
 if __name__ == '__main__':
