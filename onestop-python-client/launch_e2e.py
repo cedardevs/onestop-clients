d760ba04358e6165dc3352cd782d55adc57f72c3
Jeff Huynh <jehu0078@colorado.edu>
2021-01-29 13:05:07 -0700

    Made changes to launch_e2e.py to incorporate new changes with avro schema classes

--- onestop-python-client/launch_e2e.py
+++ onestop-python-client/launch_e2e.py
@@ -6,6 +6,10 @@ from onestop.util.S3MessageAdapter import S3MessageAdapter
 from onestop.WebPublisher import WebPublisher
 from onestop.extract.CsbExtractor import CsbExtractor
 
+from onestop.schemas.geojsonSchemaClasses.org.cedar.schemas.avro.geojson.point import Point
+from onestop.schemas.geojsonSchemaClasses.point_type import PointType
+from onestop.schemas.psiSchemaClasses.org.cedar.schemas.avro.psi.temporal_bounding import TemporalBounding
+
 
 def handler(recs):
     print("Handler...")
@@ -28,33 +32,45 @@ def handler(recs):
             print("Adding uuid")
             s3_utils.add_uuid_metadata(s3_resource, bucket, s3_key)
 
+    # Getting Spatial boundings using CSB Extractor
+    s3 = s3_utils.connect('s3', 'us-east-2')
+    raw_file_data = s3_utils.read_bytes_s3(s3, bucket, s3_key)
+
     im_message = s3ma.transform(recs)
 
-    # Add geospatial temporal bounds
-    # # Looks to see if the file is a csv file
-    if csb_extractor.is_csv(s3_key):
-        bounds_dict = csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
-        coords = bounds_dict["geospatial"]
-        min_lon = coords[0]
-        min_lat = coords[1]
-        max_lon = coords[2]
-        max_lat = coords[3]
+    for file in local_files:
+        if file in s3_key:
+            local_file = "tests/data/" + file
+            # Dont need to check if it's a csv file because we already know it is in this case
+            csb_extractor = CsbExtractor(local_file)
+            bounds_dict = csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
+            coords = bounds_dict["geospatial"]
+            min_lon = coords[0]
+            min_lat = coords[1]
+            max_lon = coords[2]
+            max_lat = coords[3]
 
-        date_rng = bounds_dict["temporal"]
-        begin_date_str = date_rng[0]
-        end_date_str = date_rng[1]
+            date_rng = bounds_dict["temporal"]
+            begin_date_str = date_rng[0]
+            end_date_str = date_rng[1]
+            tempBounding = TemporalBounding(beginDate=begin_date_str, endDate=end_date_str)
 
-        coords = csb_extractor.extract_coords(max_lon, max_lat, min_lon, min_lat)
+            coords = csb_extractor.extract_coords(max_lon, max_lat, min_lon, min_lat)
 
-    for coord in coords:
-        im_message.coordinates.append(coord)
+            pointType = PointType('Point')
+            point = Point(coordinates=coords[0], type=pointType)
 
-    im_message.temporalBounding = {'beginDate': begin_date_str, 'endDate': end_date_str}
+            # adding temp and spatial bounding to parsed record obj
+            # Storing spatial boundings as Point for now, need to create spatial bounding helper functions to support Line String, Bounding Box, etc
+            im_message.discovery.spatialBounding = point
+            im_message.discovery.temporalBounding = tempBounding
 
-    json_payload = im_message.serialize()
+    # need default str so that our enum classes don't throw an error
+    json_payload = json.dumps(im_message.to_dict(), default=str, indent=2)
     print(json_payload)
 
     registry_response = wp.publish_registry("granule", object_uuid, json_payload, "POST")
+    print('Registry Response')
     print(registry_response.json())
 
     # Upload to archive
@@ -83,6 +99,7 @@ def handler(recs):
     json_payload = json.dumps(addlocPayload, indent=2)
     # Send patch request next with archive location
     registry_response = wp.publish_registry("granule", object_uuid, json_payload, "PATCH")
+
 
 
 if __name__ == '__main__':
@@ -124,8 +141,6 @@ if __name__ == '__main__':
     sqs_consumer = SqsConsumer(conf_loc, cred_loc)
     s3ma = S3MessageAdapter("config/csb-data-stream-config.yml", s3_utils)
 
-    # Retrieve data from s3 object
-    csb_extractor = CsbExtractor()
     wp = WebPublisher("config/web-publisher-config-dev.yml", cred_loc)
 
     queue = sqs_consumer.connect()
