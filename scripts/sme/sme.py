930a2ad5d73a5d3ebaed5770df6731963ee1ace7
Jeff Huynh <jehu0078@colorado.edu>
2021-03-17 15:22:00 -0600

    Adding try/catch statement in sme.py

--- scripts/sme/sme.py
+++ scripts/sme/sme.py
@@ -28,82 +28,90 @@ def handler(key, value):
     print('Value: ' ,value)
 
     # Grabs the contents of the message and turns the dict string into a dictionary using json.loads
-    content_dict = json.loads(value['content'])
-    # Creates a checksum algorithm object based on contents of the message
-    checksum_alg = ChecksumAlgorithm(content_dict['fileInformation']['checksums'][0]['algorithm'].strip('ChecksumAlgorithm.'))
-    # assigns the checksum algorithm obj inside the content dict in order to used the from_dict method of the parsed record obj
-    content_dict['fileInformation']['checksums'][0]['algorithm'] = checksum_alg
-    parsed_record = ParsedRecord().from_dict(content_dict)
+    try:
+        content_dict = json.loads(value['content'])
+        # Creates a checksum algorithm object based on contents of the message
+        checksum_alg = ChecksumAlgorithm(
+            content_dict['fileInformation']['checksums'][0]['algorithm'].strip('ChecksumAlgorithm.'))
+        # assigns the checksum algorithm obj inside the content dict in order to used the from_dict method of the parsed record obj
+        content_dict['fileInformation']['checksums'][0]['algorithm'] = checksum_alg
+        parsed_record = ParsedRecord().from_dict(content_dict)
 
+        # Geospatial Extraction
+        # Extract the bucket key for csb_extractor object initialization
+        bucket_key = content_dict['discovery']['links'][0]['linkUrl'].split('.com/')[1]
 
+        csb_extractor = CsbExtractor(su, bucket_key)
+        if csb_extractor.is_csv(bucket_key):
+            geospatial = csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
+            begin_date, end_date = geospatial['temporal'][0], geospatial['temporal'][1]
+            max_lon, max_lat, min_lon, min_lat = geospatial['geospatial'][2], geospatial['geospatial'][3], \
+                                                 geospatial['geospatial'][0], geospatial['geospatial'][1]
+            coords = csb_extractor.extract_coords(max_lon, max_lat, min_lon, min_lat)
 
-    # Geospatial Extraction
-    # Extract the bucket key for csb_extractor object initialization
-    bucket_key = content_dict['discovery']['links'][0]['linkUrl'].split('.com/')[1]
+            # Create spatial bounding types based on the given coords
+            pointType = PointType('Point')
+            point = Point(coordinates=coords[0], type=pointType)
 
-    csb_extractor = CsbExtractor(su, bucket_key)
-    if csb_extractor.is_csv(bucket_key):
-        geospatial = csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
-        begin_date, end_date = geospatial['temporal'][0], geospatial['temporal'][1]
-        max_lon , max_lat, min_lon, min_lat = geospatial['geospatial'][2],geospatial['geospatial'][3], geospatial['geospatial'][0], geospatial['geospatial'][1]
-        coords = csb_extractor.extract_coords(max_lon , max_lat, min_lon, min_lat)
+            # Create temp bounding obj
+            tempBounding = TemporalBounding(beginDate=begin_date, endDate=end_date)
 
-        # Create spatial bounding types based on the given coords
-        pointType = PointType('Point')
-        point = Point(coordinates=coords[0], type=pointType)
+            # Update parsed record object with geospatial data
+            parsed_record.discovery.temporalBounding = tempBounding
+            parsed_record.discovery.spatialBounding = point
 
-        # Create temp bounding obj
-        tempBounding = TemporalBounding(beginDate=begin_date, endDate=end_date)
+            """
+            # Insert data into postgres
+            script = script_generation(coords[0], key)
+            postgres_insert(script)
+            """
 
-        # Update parsed record object with geospatial data
-        parsed_record.discovery.temporalBounding= tempBounding
-        parsed_record.discovery.spatialBounding= point
+        # update content dict
+        parsed_record.type = value['type']
+        content_dict = parsed_record.to_dict()
+        # reformat Relationship field
+        relationship_type = content_dict['relationships'][0]['type']['type']
+        content_dict['relationships'][0]['type'] = relationship_type
 
-        """
-        # Insert data into postgres
-        script = script_generation(coords[0], key)
-        postgres_insert(script)
-        """
+        # reformat File Locations
+        filelocation_type = content_dict['fileLocations']['type']['type']
+        content_dict['fileLocations']['type'] = filelocation_type
+        content_dict['fileLocations']['optionalAttributes'] = {}
 
-    # update content dict
-    parsed_record.type = value['type']
-    content_dict = parsed_record.to_dict()
-    # reformat Relationship field
-    relationship_type = content_dict['relationships'][0]['type']['type']
-    content_dict['relationships'][0]['type'] = relationship_type
+        # reformat Discovery
+        content_dict['discovery']['keywords'] = []
+        content_dict['discovery']['topicCategories'] = []
+        content_dict['discovery']['acquisitionInstruments'] = []
+        content_dict['discovery']['acquisitionOperations'] = []
+        content_dict['discovery']['acquisitionPlatforms'] = []
+        content_dict['discovery']['dataFormats'] = []
+        content_dict['discovery']['responsibleParties'] = []
+        content_dict['discovery']['citeAsStatements'] = []
+        content_dict['discovery']['crossReferences'] = []
+        content_dict['discovery']['largerWorks'] = []
+        content_dict['discovery']['legalConstraints'] = []
+        content_dict['discovery']['dsmmAccessibility'], content_dict['discovery']['dsmmDataIntegrity'], \
+        content_dict['discovery']['dsmmDataQualityAssessment'], content_dict['discovery']['dsmmDataQualityAssurance'], \
+        content_dict['discovery']['dsmmDataQualityControlMonitoring'], content_dict['discovery'][
+            'dsmmPreservability'] = 0, 0, 0, 0, 0, 0
+        content_dict['discovery']['dsmmProductionSustainability'], content_dict['discovery'][
+            'dsmmTransparencyTraceability'], content_dict['discovery']['dsmmUsability'], content_dict['discovery'][
+            'dsmmAverage'] = 0, 0, 0, 0.0
+        content_dict['discovery']['services'] = []
+        content_dict['discovery']['spatialBounding']['type'] = 'Point'
 
-    # reformat File Locations
-    filelocation_type = content_dict['fileLocations']['type']['type']
-    content_dict['fileLocations']['type'] = filelocation_type
-    content_dict['fileLocations']['optionalAttributes'] = {}
+        # Reformat File Information
+        content_dict['fileInformation']['checksums'][0]['algorithm'] = 'MD5'
 
-    # reformat Discovery
-    content_dict['discovery']['keywords'] = []
-    content_dict['discovery']['topicCategories'] = []
-    content_dict['discovery']['acquisitionInstruments'] = []
-    content_dict['discovery']['acquisitionOperations'] = []
-    content_dict['discovery']['acquisitionPlatforms'] = []
-    content_dict['discovery']['dataFormats'] = []
-    content_dict['discovery']['responsibleParties']= []
-    content_dict['discovery']['citeAsStatements'] = []
-    content_dict['discovery']['crossReferences'] = []
-    content_dict['discovery']['largerWorks'] = []
-    content_dict['discovery']['legalConstraints'] = []
-    content_dict['discovery']['dsmmAccessibility'], content_dict['discovery']['dsmmDataIntegrity'], content_dict['discovery']['dsmmDataQualityAssessment'], content_dict['discovery']['dsmmDataQualityAssurance'],content_dict['discovery']['dsmmDataQualityControlMonitoring'],content_dict['discovery']['dsmmPreservability']= 0,0,0,0,0,0
-    content_dict['discovery']['dsmmProductionSustainability'], content_dict['discovery']['dsmmTransparencyTraceability'], content_dict['discovery']['dsmmUsability'],content_dict['discovery']['dsmmAverage'] = 0,0,0, 0.0
-    content_dict['discovery']['services']= []
-    content_dict['discovery']['spatialBounding']['type'] = 'Point'
-
-    # Reformat File Information
-    content_dict['fileInformation']['checksums'][0]['algorithm'] = 'MD5'
-
-    # testing for granule only as of now
-    if content_dict['type'] == 'granule':
-        # Produce new information to kafka
-        kafka_publisher = KafkaPublisher("scripts/config/kafka-publisher-config-dev.yml")
-        metadata_producer = kafka_publisher.connect()
-        collection_id = parsed_record.relationships[0].id
-        kafka_publisher.publish_granule(metadata_producer, collection_id, collection_id, content_dict)
+        # testing for granule only as of now
+        if content_dict['type'] == 'granule':
+            # Produce new information to kafka
+            kafka_publisher = KafkaPublisher("scripts/config/kafka-publisher-config-dev.yml")
+            metadata_producer = kafka_publisher.connect()
+            collection_id = parsed_record.relationships[0].id
+            kafka_publisher.publish_granule(metadata_producer, collection_id, collection_id, content_dict)
+    except:
+        print('Invalid Format')
 
 
 if __name__ == '__main__':
