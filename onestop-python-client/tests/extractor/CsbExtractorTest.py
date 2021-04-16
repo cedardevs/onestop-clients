85ada2290c305218bbe93cae1cccb3e7c622b7e2
Erin <erin.rowland@colorado.edu>
2021-04-16 15:52:18 -0600

    1500-Changed CsbExtractor class constructor(adjusted documentation) to take dictionary with extra parameters allowed as well as methods within this class not to reference config but the variable that was set. Adjusted effected tests.

--- onestop-python-client/tests/extractor/CsbExtractorTest.py
+++ onestop-python-client/tests/extractor/CsbExtractorTest.py
@@ -1,35 +1,53 @@
 import unittest
+import os
+
+from moto import mock_s3
 from onestop.extract.CsbExtractor import CsbExtractor
 from onestop.util.S3Utils import S3Utils
-from tests.utils import abspath_from_relative
-
 
 class CsbExtractorTest(unittest.TestCase):
 
-    # def setUp(self):
-    #     print("Set it up!")
-    #     file_name = '../data/file4.csv'
-    #     self.csb_extractor = CsbExtractor(file_name)
-
     def setUp(self):
         print("Set it up!")
-        key = "public/NESDIS/CSB/file4.csv"
-        self.su = S3Utils( abspath_from_relative( __file__, "../../config/aws-util-config-dev.yml" ),
-                                   abspath_from_relative(__file__, "../../config/credentials.yml") )
-        self.csb_extractor = CsbExtractor(self.su, key)
+        self.root_proj_path = os.getcwd()
+        self.assertIsNotNone(self.root_proj_path)
+        self.key = "tests/data/file4.csv"
+        # Use open instead of our methodfor simplicity and reliability, plus not testing our code here.
+        self.file_obj = open(self.root_proj_path + '/' + self.key)
+
+        config_dict = {
+            "access_key": "test_access_key",
+            "secret_key": "test_secret_key",
+            "log_level": "DEBUG"
+        }
+
+        self.s3_utils = S3Utils(**config_dict)
+        self.bucket = "bucket"
+        self.region = "region"
 
     def tearDown(self):
         print("Tear it down!")
+        self.file_obj.close()
 
     def test_is_csv(self):
-        csv_str = '.csv'
-        self.assertTrue(self.csb_extractor.is_csv(self.csb_extractor.file_name))
+        self.assertTrue(CsbExtractor.is_csv("test/blah/file.csv"), "Failed to determine a csv file name was a csv file.")
 
+    def test_is_not_csv(self):
+        self.assertFalse(CsbExtractor.is_csv("test/blah/file.txt"), "Failed to determine a csv file name was not a csv file.")
 
-    def test_get_geospatial_temporal_bounds(self):
-        bounds_dict = self.csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
+    @mock_s3
+    def test_csb_SME_user_path(self):
+        # Setup bucket and file to read
+        s3 = self.s3_utils.connect('s3', self.region)
+        s3.create_bucket(Bucket=self.bucket, CreateBucketConfiguration={'LocationConstraint': self.region})
+        self.s3_utils.upload_s3(s3, self.root_proj_path + '/' + self.key, self.bucket, self.key, True)
+        self.assertTrue(self.s3_utils.read_bytes_s3(s3, self.bucket, self.key))
+
+        # This is how we would expect an external user to get the file.
+        sm_open_file = self.s3_utils.get_csv_s3(self.s3_utils.connect("session", None), self.bucket, self.key)
+
+        bounds_dict = CsbExtractor.get_spatial_temporal_bounds(sm_open_file, 'LON', 'LAT', 'TIME')
         coords = bounds_dict["geospatial"]
-        print(str(coords))
         self.assertEqual(coords[0], -96.847995)
         self.assertEqual(coords[1], 29.373065)
         self.assertEqual(coords[2], -92.747995)
@@ -39,38 +57,43 @@ class CsbExtractorTest(unittest.TestCase):
         self.assertEqual(date_rng[0], '2018-04-10T14:00:06.000Z' )
         self.assertEqual(date_rng[1], '2020-04-10T14:00:06.000Z' )
 
+    def test_get_geospatial_temporal_bounds(self):
+        bounds_dict = CsbExtractor.get_spatial_temporal_bounds(self.file_obj, 'LON', 'LAT', 'TIME')
+
+        coords = bounds_dict["geospatial"]
+        self.assertEqual(coords[0], -96.847995)
+        self.assertEqual(coords[1], 29.373065)
+        self.assertEqual(coords[2], -92.747995)
+        self.assertEqual(coords[3], 33.373065)
+
+        date_rng = bounds_dict["temporal"]
+        self.assertEqual(date_rng[0], '2018-04-10T14:00:06.000Z' )
+        self.assertEqual(date_rng[1], '2020-04-10T14:00:06.000Z' )
 
     def test_get_min_lon(self):
-        bounds_dict = self.csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
+        bounds_dict = CsbExtractor.get_spatial_temporal_bounds(self.file_obj, 'LON', 'LAT', 'TIME')
+
         coords = bounds_dict["geospatial"]
         min_lon = coords[0]
         self.assertEqual(min_lon, -96.847995)
 
-
     def test_get_max_datetime(self):
-        bounds_dict = self.csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
+
+        bounds_dict = CsbExtractor.get_spatial_temporal_bounds(self.file_obj, 'LON', 'LAT', 'TIME')
+
         date_rng = bounds_dict["temporal"]
         end_date = date_rng[1]
         self.assertEqual(end_date, '2020-04-10T14:00:06.000Z')
 
-
     def test_get_min_datetime(self):
-        bounds_dict = self.csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
+        bounds_dict = CsbExtractor.get_spatial_temporal_bounds(self.file_obj, 'LON', 'LAT', 'TIME')
+
         date_rng = bounds_dict["temporal"]
         begin_date = date_rng[0]
         self.assertEqual(begin_date, '2018-04-10T14:00:06.000Z')
 
-
     def test_extract_coords(self):
-        bounds_dict = self.csb_extractor.get_spatial_temporal_bounds('LON', 'LAT', 'TIME')
-        coords = bounds_dict["geospatial"]
-
-        min_lon = coords[0]
-        min_lat = coords[1]
-        max_lon = coords[2]
-        max_lat = coords[3]
-
-        coords = self.csb_extractor.extract_coords(max_lon, max_lat, min_lon, min_lat)
+        coords = CsbExtractor.extract_coords(self.file_obj, -92.747995, 33.373065, -96.847995, 29.373065)
         result = [[
             -94.847995,
             29.373065
