1e629ab31e343e52c3fc93f839b26dc566e710a0
Erin <erin.rowland@colorado.edu>
2021-04-28 10:08:53 -0600

    1500-Changed KafkaConsumer class constructors(adjusted documentation) to take dictionary with extra parameters allowed as well as methods within this class not to reference config but the variable that was set. Adjusted effected tests. Removed get_logger method as it wasn't used and we used a different logger then. Added checks for if security wasn't enabled.

--- onestop-python-client/onestop/KafkaConsumer.py
+++ onestop-python-client/onestop/KafkaConsumer.py
@@ -1,11 +1,9 @@
-import logging
-import yaml
-
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.error import KafkaError
 from confluent_kafka import DeserializingConsumer
 from confluent_kafka.schema_registry.avro import AvroDeserializer
 from confluent_kafka.serialization import StringDeserializer
+from onestop.util.ClientLogger import ClientLogger
 
 class KafkaConsumer:
     """
@@ -13,109 +11,97 @@ class KafkaConsumer:
 
     Attributes
     ----------
-    conf: yaml file
-        kafka-publisher-config-dev.yml
-    logger: Logger object
-            utilizes python logger library and creates logging for our specific needs
-    logger.info: Logger object
-        logging statement that occurs when the class is instantiated
-    metadata_type: str
-        type of metadata (COLLECTION or GRANULE)
-    brokers: str
-        brokers (kubernetes service)
-    group_id: str
-        Client group id string. All clients sharing the same group.id belong to the same group
-    auto_offset_reset: str
-        Action to take when there is no initial offset in offset store or the desired offset is out of range (smallest, earliest, beginning, largest, latest, end, error)
-    schema_registry: str
-        schema registry (kubernetes service)
-    security: boolean
-        defines if security is in place
-    collection_topic: str
-        collection topic you want to consume
-    granule_topic: str
-        granule topic you want to consume
+        metadata_type: str
+            type of metadata (COLLECTION or GRANULE)
+        brokers: str
+            brokers (kubernetes service)
+        group_id: str
+            Client group id string. All clients sharing the same group.id belong to the same group
+        auto_offset_reset: str
+            Action to take when there is no initial offset in offset store or the desired offset is out of range (smallest, earliest, beginning, largest, latest, end, error)
+        schema_registry: str
+            schema registry (kubernetes service)
+        security_enabled: boolean
+            Whether to use security for the kafka schema registry client.
+        security_caLoc: str
+            Kafka schema registry certification authority (CA) file location.
+        security_keyLoc: str
+            Kafka schema registry client's private key file location.
+        security_certLoc: str
+            Kafka schema registry client's public key file location.
+        collection_topic_consume: str
+            collection topic you want to consume
+        granule_topic_consume: str
+            granule topic you want to consume
+        logger: Logger object
+                utilizes python logger library and creates logging for our specific needs
 
     Methods
     -------
-    get_logger(log_name, create_file)
-        creates logger file
+        register_client()
+            registers to schema registry client based on configs
 
-    register_client()
-        registers to schema registry client based on configs
+        connect()
+            utilizes register_client() and create_consumer(registry_client) to connect to schema registry and allow for consumption of topics
 
-    create_consumer(registry_client)
-        subscribes to topic defined in configs and creates a consumer to deserialize messages from topic
+        create_consumer(registry_client)
+            subscribes to topic defined in configs and creates a consumer to deserialize messages from topic
 
-    connect()
-        utilizes register_client() and create_consumer(registry_client) to connect to schema registry and allow for consumption of topics
-
-    consume(metadata_consumer, handler)
-        asynchronously polls for messages in the connected topic, results vary depending on the handler function that is passed into it
+        consume(metadata_consumer, handler)
+            asynchronously polls for messages in the connected topic, results vary depending on the handler function that is passed into it
     """
-    conf = None
 
-    def __init__(self, conf_loc):
-        with open(conf_loc) as f:
-            self.conf = yaml.load(f, Loader=yaml.FullLoader)
+    def __init__(self, metadata_type, brokers, group_id, auto_offset_reset, schema_registry, security, collection_topic_consume, granule_topic_consume, log_level = 'INFO', **wildargs):
+        """
+        Attributes
+        ----------
+            metadata_type: str
+                type of metadata (COLLECTION or GRANULE)
+            brokers: str
+                brokers (kubernetes service)
+            group_id: str
+                Client group id string. All clients sharing the same group.id belong to the same group
+            auto_offset_reset: str
+                Action to take when there is no initial offset in offset store or the desired offset is out of range (smallest, earliest, beginning, largest, latest, end, error)
+            schema_registry: str
+                schema registry (kubernetes service) URL
+            security: dict
+                enabled boolean: Whether to use security for kafka schema registry client.
+                caLoc str: Kafka schema registry certification authority (CA) file location.
+                keyLoc str: Kafka schema registry client's private key file location.
+                certLoc str: Kafka schema registry client's public key file location.
 
-        self.logger = self.get_logger(self.__class__.__name__, False)
-        self.logger.info("Initializing " + self.__class__.__name__)
-        self.metadata_type = self.conf['metadata_type']
-        self.brokers = self.conf['brokers']
-        self.group_id = self.conf['group_id']
-        self.auto_offset_reset = self.conf['auto_offset_reset']
-        self.schema_registry = self.conf['schema_registry']
-        self.security = self.conf['security']['enabled']
+            collection_topic_consume: str
+                collection topic you want to consume
+            granule_topic_consume: str
+                granule topic you want to consume
+            log_level: str
+                    What log level to use for this class
+        """
 
-        self.collection_topic = self.conf['collection_topic_consume']
-        self.granule_topic = self.conf['granule_topic_consume']
+        self.metadata_type = metadata_type
+        self.brokers = brokers
+        self.group_id = group_id
+        self.auto_offset_reset = auto_offset_reset
+        self.schema_registry = schema_registry
+        self.security_enabled = security['enabled']
+
+        if self.security_enabled:
+            self.security_caLoc = security['caLoc']
+            self.security_keyLoc = security['keyLoc']
+            self.security_certLoc = security['certLoc']
+
+        self.collection_topic_consume = collection_topic_consume
+        self.granule_topic_consume = granule_topic_consume
 
         if self.metadata_type not in ['COLLECTION', 'GRANULE']:
             raise ValueError("metadata_type must be 'COLLECTION' or 'GRANULE'")
 
-    def get_logger(self, log_name, create_file):
-        """
-        Utilizes python logger library and creates logging
+        self.logger = ClientLogger.get_logger(self.__class__.__name__, log_level, False)
+        self.logger.info("Initializing " + self.__class__.__name__)
 
-        :param log_name: str
-            name of log to be created
-        :param create_file: boolean
-            defines whether of not you want a logger file to be created
-
-        :return: Logger object
-        """
-
-        # create logger
-        log = logging.getLogger()
-
-        # create formatter and add it to the handlers
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-        if self.conf['log_level'] == "DEBUG":
-            log.setLevel(level=logging.DEBUG)
-        else:
-            if self.conf['log_level'] == "INFO":
-                log.setLevel(level=logging.INFO)
-            else:
-                log.setLevel(level=logging.ERROR)
-
-        fh = None
-        if create_file:
-            # create file handler for logger.
-            fh = logging.FileHandler(log_name)
-            fh.setFormatter(formatter)
-
-        # create console handler for logger.
-        ch = logging.StreamHandler()
-        ch.setFormatter(formatter)
-
-        # add handlers to logger.
-        if create_file:
-            log.addHandler(fh)
-
-        log.addHandler(ch)
-        return log
+        if wildargs:
+            self.logger.error("There were extra constructor arguments: " + str(wildargs))
 
     def register_client(self):
         """
@@ -125,10 +111,10 @@ class KafkaConsumer:
         """
         reg_conf = {'url': self.schema_registry}
 
-        if self.security:
-            reg_conf['ssl.ca.location'] = self.conf['security']['caLoc']
-            reg_conf['ssl.key.location'] = self.conf['security']['keyLoc']
-            reg_conf['ssl.certificate.location'] = self.conf['security']['certLoc']
+        if self.security_enabled:
+            reg_conf['ssl.ca.location'] = self.security_caLoc
+            reg_conf['ssl.key.location'] = self.security_keyLoc
+            reg_conf['ssl.certificate.location'] = self.security_certLoc
 
         registry_client = SchemaRegistryClient(reg_conf)
         return registry_client
@@ -166,11 +152,11 @@ class KafkaConsumer:
 
         consumer_conf = {'bootstrap.servers': self.brokers}
 
-        if self.security:
+        if self.security_enabled:
             consumer_conf['security.protocol'] = 'SSL'
-            consumer_conf['ssl.ca.location'] = self.conf['security']['caLoc']
-            consumer_conf['ssl.key.location'] = self.conf['security']['keyLoc']
-            consumer_conf['ssl.certificate.location'] = self.conf['security']['certLoc']
+            consumer_conf['ssl.ca.location'] = self.security_caLoc
+            consumer_conf['ssl.key.location'] = self.security_keyLoc
+            consumer_conf['ssl.certificate.location'] = self.security_certLoc
 
         meta_consumer_conf = consumer_conf
         meta_consumer_conf['key.deserializer'] = StringDeserializer('utf-8')
---
+++ onestop-python-client/tests/KafkaConsumerTest.py
@@ -0,0 +1,264 @@
+import unittest
+
+from unittest.mock import ANY, patch, MagicMock, call
+from onestop.KafkaConsumer import KafkaConsumer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.serialization import StringDeserializer
+
+class KafkaConsumerTest(unittest.TestCase):
+    kp = None
+    conf_w_security = None
+    conf_wo_security = None
+
+    @classmethod
+    def setUp(cls):
+        print("Set it up!")
+        cls.conf_w_security = {
+            "metadata_type" : "GRANULE",
+            "brokers" : "onestop-dev-cp-kafka:9092",
+            "group_id" : "sme-test",
+            "auto_offset_reset" : "earliest",
+            "schema_registry" : "http://onestop-dev-cp-schema-registry:8081",
+            "security" : {
+                "enabled" : True,
+                "caLoc" : "/etc/pki/tls/cert.pem",
+                "keyLoc" : "/etc/pki/tls/private/kafka-user.key",
+                "certLoc" : "/etc/pki/tls/certs/kafka-user.crt"
+            },
+            "collection_topic_consume" : "psi-collection-input-unknown",
+            "granule_topic_consume" : "psi-granule-input-unknown",
+            "log_level" : "DEBUG"
+        }
+        cls.conf_wo_security = dict(cls.conf_w_security)
+        # Remove security credential section.
+        cls.conf_wo_security['security'] = {
+            "enabled":False
+        }
+
+    @classmethod
+    def tearDown(self):
+        print("Tear it down!")
+
+    def test_init_happy_nonconditional_params(self):
+        consumer = KafkaConsumer(**self.conf_w_security)
+
+        self.assertEqual(consumer.metadata_type, self.conf_w_security['metadata_type'])
+        self.assertEqual(consumer.brokers, self.conf_w_security['brokers'])
+        self.assertEqual(consumer.group_id, self.conf_w_security['group_id'])
+        self.assertEqual(consumer.auto_offset_reset, self.conf_w_security['auto_offset_reset'])
+        self.assertEqual(consumer.schema_registry, self.conf_w_security['schema_registry'])
+        self.assertEqual(consumer.security_enabled, self.conf_w_security['security']['enabled'])
+        self.assertEqual(consumer.collection_topic_consume, self.conf_w_security['collection_topic_consume'])
+        self.assertEqual(consumer.granule_topic_consume, self.conf_w_security['granule_topic_consume'])
+
+    def test_init_security_enabled(self):
+        consumer = KafkaConsumer(**self.conf_w_security)
+
+        self.assertEqual(consumer.security_caLoc, self.conf_w_security['security']['caLoc'])
+        self.assertEqual(consumer.security_keyLoc, self.conf_w_security['security']['keyLoc'])
+        self.assertEqual(consumer.security_certLoc, self.conf_w_security['security']['certLoc'])
+
+    def test_init_security_disabled(self):
+        consumer = KafkaConsumer(**self.conf_wo_security)
+
+        self.assertRaises(AttributeError, getattr, consumer, "security_caLoc")
+        self.assertRaises(AttributeError, getattr, consumer, "security_keyLoc")
+        self.assertRaises(AttributeError, getattr, consumer, "security_certLoc")
+
+    def test_init_metadata_type_valid(self):
+        consumer = KafkaConsumer(**self.conf_w_security)
+
+        self.assertEqual(consumer.metadata_type, self.conf_w_security['metadata_type'])
+
+    def test_init_metadata_type_invalid(self):
+        wrong_metadata_type_config = dict(self.conf_w_security)
+        wrong_metadata_type_config['metadata_type'] = "invalid_type"
+
+        self.assertRaises(ValueError, KafkaConsumer, **wrong_metadata_type_config)
+
+    @patch.object(SchemaRegistryClient, '__init__', autospec=True)
+    def test_register_client_w_security(self, mock_client):
+        schema_conf = {
+            'url':self.conf_w_security['schema_registry'],
+            'ssl.ca.location': self.conf_w_security['security']['caLoc'],
+            'ssl.key.location': self.conf_w_security['security']['keyLoc'],
+            'ssl.certificate.location': self.conf_w_security['security']['certLoc']
+        }
+        mock_client.return_value = None
+
+        consumer = KafkaConsumer(**self.conf_w_security)
+        consumer.register_client()
+
+        mock_client.assert_called()
+        mock_client.assert_called_with(ANY, schema_conf)
+
+    @patch.object(SchemaRegistryClient, '__init__', autospec=True)
+    def test_register_client_wo_security(self, mock_client):
+        schema_conf = {
+            'url' : self.conf_wo_security['schema_registry']
+        }
+        mock_client.return_value = None
+
+        consumer = KafkaConsumer(**self.conf_wo_security)
+        consumer.register_client()
+
+        mock_client.assert_called_with(ANY, schema_conf)
+
+    @patch('onestop.KafkaConsumer.AvroDeserializer')
+    @patch('onestop.KafkaConsumer.DeserializingConsumer')
+    def test_create_consumer_calls_AvroDeserializer(self, mock_deserializing_consumer, mock_avro_deserializer):
+        conf_w_security_collection = dict(self.conf_w_security)
+        conf_w_security_collection['metadata_type'] = "COLLECTION"
+
+        consumer = KafkaConsumer(**conf_w_security_collection)
+        reg_client = consumer.register_client()
+        reg_client.get_latest_version = MagicMock()
+        consumer.create_consumer(reg_client)
+
+        # Verify AvroDeserializer called with expected registry client
+        mock_avro_deserializer.assert_called_with(ANY, reg_client)
+
+    @patch('onestop.KafkaConsumer.AvroDeserializer')
+    @patch('onestop.KafkaConsumer.DeserializingConsumer')
+    def test_create_consumer_collection_w_security(self, mock_deserializing_consumer, mock_avro_deserializer):
+        conf_w_security_collection = dict(self.conf_w_security)
+        topic = conf_w_security_collection['collection_topic_consume']
+        conf_w_security_collection['metadata_type'] = 'COLLECTION'
+
+        consumer = KafkaConsumer(**conf_w_security_collection)
+        reg_client = MagicMock()
+        consumer.create_consumer(reg_client)
+
+        # Verify metadata type was taken into consideration for getting topic information
+        reg_client.get_latest_version.assert_called_with(topic + '-value')
+
+        # Verify security passed into DeserializingConsumer
+        mock_deserializing_consumer.assert_called_with({'bootstrap.servers': conf_w_security_collection['brokers'],
+                                                        'security.protocol': 'SSL',
+                                                        'ssl.ca.location': conf_w_security_collection['security']['caLoc'],
+                                                        'ssl.key.location': conf_w_security_collection['security']['keyLoc'],
+                                                        'ssl.certificate.location': conf_w_security_collection['security']['certLoc'],
+                                                        'key.deserializer': ANY,
+                                                        'value.deserializer': ANY,
+                                                        'group.id': conf_w_security_collection['group_id'],
+                                                        'auto.offset.reset': conf_w_security_collection['auto_offset_reset']
+                                                        })
+        mock_deserializing_consumer.return_value.subscribe.assert_called_with([topic])
+
+    @patch('onestop.KafkaConsumer.AvroDeserializer')
+    @patch('onestop.KafkaConsumer.DeserializingConsumer')
+    def test_create_consumer_collection_wo_security(self, mock_deserializing_consumer, mock_avro_deserializer):
+        conf_wo_security_collection = dict(self.conf_wo_security)
+        topic = conf_wo_security_collection['collection_topic_consume']
+        conf_wo_security_collection['metadata_type'] = 'COLLECTION'
+
+        consumer = KafkaConsumer(**conf_wo_security_collection)
+        reg_client = MagicMock()
+        consumer.create_consumer(reg_client)
+
+        # Verify metadata type was taken into consideration for getting topic information
+        reg_client.get_latest_version.assert_called_with(topic + '-value')
+
+        # Verify no security passed into DeserializingConsumer
+        mock_deserializing_consumer.assert_called_with({'bootstrap.servers': conf_wo_security_collection['brokers'],
+                                                        'key.deserializer': ANY,
+                                                        'value.deserializer': ANY,
+                                                        'group.id': conf_wo_security_collection['group_id'],
+                                                        'auto.offset.reset': conf_wo_security_collection['auto_offset_reset']
+                                                        })
+        mock_deserializing_consumer.return_value.subscribe.assert_called_with([topic])
+
+    @patch('onestop.KafkaConsumer.AvroDeserializer')
+    @patch('onestop.KafkaConsumer.DeserializingConsumer')
+    def test_create_consumer_granule_w_security(self, mock_deserializing_consumer, mock_avro_deserializer):
+        conf_w_security_granule = dict(self.conf_w_security)
+        topic = conf_w_security_granule['granule_topic_consume']
+        conf_w_security_granule['metadata_type'] = 'GRANULE'
+
+        consumer = KafkaConsumer(**conf_w_security_granule)
+        reg_client = MagicMock()
+        consumer.create_consumer(reg_client)
+
+        # Verify metadata type was taken into consideration for getting topic information
+        reg_client.get_latest_version.assert_called_with(topic + '-value')
+
+        # Verify security passed into DeserializingConsumer
+        mock_deserializing_consumer.assert_called_with({'bootstrap.servers': conf_w_security_granule['brokers'],
+                                                        'security.protocol': 'SSL',
+                                                        'ssl.ca.location': conf_w_security_granule['security']['caLoc'],
+                                                        'ssl.key.location': conf_w_security_granule['security']['keyLoc'],
+                                                        'ssl.certificate.location': conf_w_security_granule['security']['certLoc'],
+                                                        'key.deserializer': ANY,
+                                                        'value.deserializer': ANY,
+                                                        'group.id': conf_w_security_granule['group_id'],
+                                                        'auto.offset.reset': conf_w_security_granule['auto_offset_reset']
+                                                        })
+        mock_deserializing_consumer.return_value.subscribe.assert_called_with([topic])
+
+    @patch('onestop.KafkaConsumer.AvroDeserializer')
+    @patch('onestop.KafkaConsumer.DeserializingConsumer')
+    def test_create_consumer_granule_wo_security(self, mock_deserializing_consumer, mock_avro_deserializer):
+        conf_wo_security_granule = dict(self.conf_wo_security)
+        topic = conf_wo_security_granule['granule_topic_consume']
+        conf_wo_security_granule['metadata_type'] = 'GRANULE'
+
+        # Verify security taken into consideration
+        meta_consumer_conf = {'bootstrap.servers': conf_wo_security_granule['brokers'],
+                              'key.deserializer': StringDeserializer('utf-8'),
+                              'value.deserializer': mock_avro_deserializer,
+                              'group.id': conf_wo_security_granule['group_id'],
+                              'auto.offset.reset': conf_wo_security_granule['auto_offset_reset']
+                              }
+
+        consumer = KafkaConsumer(**conf_wo_security_granule)
+        reg_client = MagicMock()
+        consumer.create_consumer(reg_client)
+
+        # Verify metadata type was taken into consideration for getting topic information
+        reg_client.get_latest_version.assert_called_with(topic + '-value')
+
+        # Verify no security passed into DeserializingConsumer called with expected configuration
+        meta_consumer_conf['key.deserializer'] = ANY
+        meta_consumer_conf['value.deserializer'] = ANY
+        mock_deserializing_consumer.assert_called_with(meta_consumer_conf)
+        mock_deserializing_consumer.return_value.subscribe.assert_called_with([topic])
+
+    def test_connect(self):
+        mock_client = MagicMock()
+
+        consumer = KafkaConsumer(**self.conf_w_security)
+        consumer.register_client = MagicMock(return_value=mock_client)
+        consumer.create_consumer = MagicMock(return_value=MagicMock(mock_client))
+        consumer.connect()
+
+        consumer.register_client.assert_called_once()
+        consumer.create_consumer.assert_called_with(mock_client)
+
+    @patch('confluent_kafka.cimpl.Message')
+    @patch('onestop.KafkaConsumer.DeserializingConsumer')
+    def test_consume(self, mock_metadata_consumer, mock_message):
+        mock_message_key = 'key1'
+        mock_message_value = 'value1'
+        consumer = KafkaConsumer(**self.conf_w_security)
+        consumer.register_client = MagicMock(return_value=MagicMock())
+        mock_message.key.return_value = mock_message_key
+        mock_message.value.return_value = mock_message_value
+        mock_metadata_consumer.poll.side_effect = [None, mock_message, Exception]
+        mock_handler = MagicMock()
+
+        # Would have liked not having the try/catch but it wasn't ignoring the exception. Just need to not fail due to end of loop.
+        try:
+            self.assertRaises(Exception, consumer.consume(mock_metadata_consumer, mock_handler))
+        except Exception as e:
+            print("Ignoring exception: {}".format(e))
+
+        # Verify kafka consumer poll called expected number of times
+        self.assertTrue(mock_metadata_consumer.poll.call_count == 3)
+        mock_metadata_consumer.poll.assert_has_calls([call(10), call(10), call(10)])
+
+        # Verify callback function was called once with expected message attributes
+        mock_handler.assert_called_once()
+        mock_handler.assert_called_with(mock_message_key, mock_message_value)
+
+    if __name__ == '__main__':
+        unittest.main()
