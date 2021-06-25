e44e1354569fc6a862e00de8de8260acc50681a1
Erin <erin.rowland@colorado.edu>
2021-06-25 11:58:59 -0600

    1508-Removed unused avro tests since looks like an import is no longer available. Never worked for our code, would need work.

--- onestop-python-client/onestop/schemas/geojsonSchemaClasses_test/org/cedar/schemas/avro/geojson/__init__.py
+++
@@ -1,6 +0,0 @@
-from onestop.schemaTest2_test.org.cedar.schemas.avro.geojson.line_string_factory import LineStringFactory
-from onestop.schemaTest2_test.org.cedar.schemas.avro.geojson.polygon_factory import PolygonFactory
-from onestop.schemaTest2_test.org.cedar.schemas.avro.geojson.multi_line_string_factory import MultiLineStringFactory
-from onestop.schemaTest2_test.org.cedar.schemas.avro.geojson.point_factory import PointFactory
-from onestop.schemaTest2_test.org.cedar.schemas.avro.geojson.multi_point_factory import MultiPointFactory
-from onestop.schemaTest2_test.org.cedar.schemas.avro.geojson.multi_polygon_factory import MultiPolygonFactory
