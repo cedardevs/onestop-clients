c3a22c8de52cf0aeb622bb6d9cc2f41dedc9ec6f
Erin <erin.rowland@colorado.edu>
2021-07-12 12:51:13 -0600

    1508-Changed test_jsonEncoder tests to not need manually typing the class name in the assertion.

--- onestop-python-client/test/unit/schemas/util/test_jsonEncoder.py
+++ onestop-python-client/test/unit/schemas/util/test_jsonEncoder.py
@@ -19,49 +19,49 @@ from onestop.schemas.geojsonSchemaClasses.polygon_type import PolygonType
 class jsonEncoderTest(unittest.TestCase):
 
     def test_checksumalgorithm_enum_class_encodes(self):
-        type = ChecksumAlgorithm.MD5.value
+        type = ChecksumAlgorithm.MD5
         obj = ChecksumAlgorithm(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "ChecksumAlgorithm.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_relationshiptype_enum_class_encodes(self):
-        type = RelationshipType.collection.value
+        type = RelationshipType.collection
         obj = RelationshipType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "RelationshipType.%s"}'%type.lower())
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_recordtype_enum_class_encodes(self):
-        type = RecordType.granule.value
+        type = RecordType.granule
         obj = RecordType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "RecordType.%s"}'%type.lower())
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_timerangedescriptor_enum_class_encodes(self):
-        type = TimeRangeDescriptor.AMBIGUOUS.value
+        type = TimeRangeDescriptor.AMBIGUOUS
         obj = TimeRangeDescriptor(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "TimeRangeDescriptor.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_linestring_enum_class_encodes(self):
-        type = LineStringType.LineString.value
+        type = LineStringType.LineString
         obj = LineStringType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "LineStringType.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_method_enum_class_encodes(self):
         type = Method.CONNECT
@@ -73,64 +73,64 @@ class jsonEncoderTest(unittest.TestCase):
         self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_multilinestringtype_enum_class_encodes(self):
-        type = MultiLineStringType.MultiLineString.value
+        type = MultiLineStringType.MultiLineString
         obj = MultiLineStringType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "MultiLineStringType.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_multipointtype_enum_class_encodes(self):
-        type = MultiPointType.MultiPoint.value
+        type = MultiPointType.MultiPoint
         obj = MultiPointType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "MultiPointType.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_multipolygontype_enum_class_encodes(self):
-        type = MultiPolygonType.MultiPolygon.value
+        type = MultiPolygonType.MultiPolygon
         obj = MultiPolygonType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "MultiPolygonType.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_pointtype_enum_class_encodes(self):
-        type = PointType.Point.value
+        type = PointType.Point
         obj = PointType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "PointType.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_polygontype_enum_class_encodes(self):
-        type = PolygonType.Polygon.value
+        type = PolygonType.Polygon
         obj = PolygonType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "PolygonType.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_filelocationtype_enum_class_encodes(self):
-        type = FileLocationType.INGEST.value
+        type = FileLocationType.INGEST
         obj = FileLocationType(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "FileLocationType.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
 
     def test_validdescriptor_enum_class_encodes(self):
-        type = ValidDescriptor.INVALID.value
+        type = ValidDescriptor.INVALID
         obj = ValidDescriptor(type)
 
         jsonStr = json.dumps(obj,
                              cls=EnumEncoder)
 
-        self.assertEqual(jsonStr, '{"__enum__": "ValidDescriptor.%s"}'%type)
+        self.assertEqual(jsonStr, '{"__enum__": "%s"}'%type)
